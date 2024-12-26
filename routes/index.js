ea0110a6353f138603362d97bfe8457c4e02634b
Nishant Sharma <135991298+Nishantsharma1243@users.noreply.github.com>
2024-12-26 21:36:22 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('admin_products');
+    res.render('user_login');
 })
 
 router.get('/map/:orderid',function (req, res) {
